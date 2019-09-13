8184f72415224f5bde09778c2babdc243641a9ed
kalek91 <cabelein@rams.colostate.edu>
2019-09-13 12:52:39 -0600

    Fixed About display. close #65

--- client/src/components/Application/About/About.js
+++ client/src/components/Application/About/About.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
-import {CardGroup, Card, CardHeader, CardBody, CardImg, CardTitle, CardText} from 'reactstrap';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardImg,
+  CardTitle,
+  CardText,
+  Row, Col, Container
+} from 'reactstrap';
 import cabeleinPhoto from './images/crabelein.jpg'
 import dbacaPhoto from './images/dbaca.jpg'
 import jamesl84Photo from './images/Jamesl84.jpg'
@@ -8,25 +16,53 @@ import hgqPhoto from './images/hqMongoose.jpg'
 
 export default class About extends Component {
   render() {
+    let team = [[cabeleinPhoto, "Christopher Abelein",
+                  "a CS senior."],
+                [dbacaPhoto, "Damien Baca",
+                  "a CS Senior with a Math Minor"],
+                [jamesl84Photo, "James Lounsbury",
+                  "A CS and Physics major"],
+                [longChenPhoto, "Long Chen",
+                  "a ME major."],
+                [hgqPhoto, "Hayden Quintana",
+                  "likes long walks on the beach and poking dead things with a stick"]
+                ];
     return (
-      <CardGroup>
-        {this.showCard(cabeleinPhoto, "Christopher Abelein",
-            "a CS senior.")}
-        {this.showCard(dbacaPhoto, "Damien Baca",
-            "a CS Senior with a Math Minor")}
-        {this.showCard(jamesl84Photo, "James Lounsbury",
-            "A CS and Physics major")}
-        {this.showCard(longChenPhoto, "Long Chen",
-            "a ME major.")}
-        {this.showCard(hgqPhoto, "Hayden Quintana",
-        "likes long walks on the beach and poking dead things with a stick")}
-      </CardGroup>
+        this.generateCardGroups(team)
+    );
+  }
+
+  generateCardGroups(team) {
+    return (
+        <Container>
+          <Row>
+            <Col xs="12" sm="12" md="6" lg="4" xl="3">
+              {this.showCard(team[0][0], team[0][1], team[0][2])}
+            </Col>
+            <Col xs="12" sm="12" md="6" lg="4" xl="3">
+              {this.showCard(team[1][0], team[1][1], team[1][2])}
+            </Col>
+            <Col xs="12" sm="12" md="6" lg="4" xl="3">
+              {this.showCard(team[2][0], team[2][1], team[2][2])}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="12" sm="12" md="6" lg="4" xl="3">
+              {this.showCard(team[3][0], team[3][1], team[3][2])}
+            </Col>
+            <Col xs="12" sm="12" md="6" lg="4" xl="3">
+              {this.showCard(team[4][0], team[4][1], team[4][2])}
+            </Col>
+            <Col xs="12" sm="12" md="6" lg="4" xl="3">
+            </Col>
+          </Row>
+        </Container>
     );
   }
 
   showCard(img, title, text) {
     return (
-        <Card>
+        <Card style={{ minWidth: '8rem' }}>
           <CardHeader>
             <CardImg top src={img}/>
           </CardHeader>
@@ -38,4 +74,3 @@ export default class About extends Component {
     );
   }
 }
-
