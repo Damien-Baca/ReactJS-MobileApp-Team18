d0380ee1e33760a7398a0935a6ba96b2bbdaa9e6
jameslouns <james.lounsbury0804@gmail.com>
2019-10-02 22:17:29 -0600

    Added imports needed for tests. Created member variables for testing, then in the before section initialized origin and destination. Finally added first test in Test section called testSameLocation that verifies that if you give the same origin and destination that the distance equals zero.

--- server/src/test/java/com/tripco/t18/misc/TestGreatCircleDistance.java
+++ server/src/test/java/com/tripco/t18/misc/TestGreatCircleDistance.java
@@ -1,7 +1,34 @@
 package com.tripco.t18.misc;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestGreatCircleDistance {
+  private final float earthRadiusMiles=3959;
+  private final float earthRadiusKilometer= 6371;
+  private Map<String, Object> origin;
+  private Map<String, Object> destination;
+  private GreatCircleDistance test = new GreatCircleDistance();
 
+  @Before
+  public void createLocations(){
+    origin=new HashMap<>();
+    origin.put("latitude","");
+    origin.put("longitude","");
+    destination=new HashMap<>();
+    destination.put("latitude","");
+    destination.put("longitude","");
+  }
 
+  @Test
+  public void testSameLocation(){
+    origin.replace("latitude","40");
+    origin.replace("longitude","-105");
+    int expect=0;
+    int actual=test.calculateDistance(origin,origin,earthRadiusMiles);
+    assertEquals("traveled nowhere",expect,actual);
+  }
 }
