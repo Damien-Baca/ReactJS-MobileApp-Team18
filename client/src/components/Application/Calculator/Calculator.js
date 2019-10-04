4559cc7b4fbbead790e84632e607daedb07b7471
hgq970 <hayden.quintana970@gmail.com>
2019-10-03 18:29:45 -0600

    calculator coordinate validation patch 03 working

--- client/src/components/Application/Calculator/Calculator.js
+++ client/src/components/Application/Calculator/Calculator.js
@@ -19,12 +19,16 @@ export default class Calculator extends Component {
       distance: 0,
       errorMessage: null,
       valid: {
-        originlatitude: false, originlongitude: false,
-        destinationatitude: false, destinationlongitude: false
+        originlatitude: false,
+        originlongitude: false,
+        destinationatitude: false,
+        destinationlongitude: false
       },
       invalid: {
-        originlatitude: false, originlongitude: false,
-        destinationlatitude: false, destinationlongitude: false
+        originlatitude: false,
+        originlongitude: false,
+        destinationlatitude: false,
+        destinationlongitude: false
       },
     };
   }
@@ -75,6 +79,8 @@ export default class Calculator extends Component {
                id={`${stateVar}${capitalizedCoordinate}`}
                value={this.state[stateVar][coordinate]}
                onChange={updateStateVarOnChange}
+               valid={this.state.valid[stateVar+coordinate]}
+               invalid={this.state.invalid[stateVar+coordinate]}
                style={{width: "100%"}}/>
     );
 
@@ -99,7 +105,7 @@ export default class Calculator extends Component {
                 <div>
                   <h5>{this.state.distance} {this.props.options.activeUnit}</h5>
                   <Button onClick={this.calculateDistance}
-                          disabled={ !(this.state.valid.origin.latitude && this.state.valid.origin.longitude && this.state.valid.destination.latitude && this.state.valid.destination.longitude) } //TODO
+                          disabled={ !(this.state.valid.originlatitude && this.state.valid.originlongitude && this.state.valid.destinationlatitude && this.state.valid.destinationlongitude) } //TODO
                   >Calculate</Button>
                 </div>}
         />
@@ -136,14 +142,19 @@ export default class Calculator extends Component {
   }
 
   updateLocationOnChange(stateVar, field, value){ //origindest, target.name, target.value
-
+    console.log('update');
     if (value === '') { //empty
+      console.log('empty');
       this.setValidState(stateVar, field, value, false, false);
 
     } else if (this.props.validation(field, value) ) { //if coord is good
+      console.log('valid');
       this.setValidState(stateVar, field, value, true, false);
+
     } else { //bad coord
+      console.log('invalid');
       this.setValidState(stateVar, field, value, false, true);
+
     }
   }
 
@@ -153,11 +164,12 @@ export default class Calculator extends Component {
     location[field] = value;
     //console.log(location);
     let validClone = Object.assign({}, this.state.valid);
-    validClone[stateVar][field] = valid;
+    validClone[stateVar+field] = valid;
 
-    let invalidClone = Object.assign({}, this.state.valid);
-    invalidClone[stateVar][field] = invalid;
+    let invalidClone = Object.assign({}, this.state.invalid);
+    invalidClone[stateVar+field] = invalid;
 
+    console.log(validClone, invalidClone);
 
     this.setState({
       [stateVar]: location,
