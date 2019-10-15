dd8e80b973cc42771da635997833885dd9ad3466
jacky <chenlong890729@gmail.com>
2019-10-15 15:11:35 -0600

    Testing generateCardGroups function with empty list in About.js

--- client/test/About.test.js
+++ client/test/About.test.js
@@ -3,3 +3,12 @@ import React from 'react'
 import {mount} from 'enzyme'
 import About from '../src/components/Application/About/About';
 
+function testgenerateCardGroupsEmptylist(){
+  let emptyObj={}
+
+  const exampleWrapper= mount(<About/>);
+  let actualResult=exampleWrapper.instance().generateCardGroups(emptyObj);
+
+  expect(actualResult)===0;
+}
+test(`Testing the generateCardGroups function with an empty list`,testgenerateCardGroupsEmptylist);
