ee02e1bc003a2a41d8505fc74a21bb3143fc63a7
jacky <chenlong890729@gmail.com>
2019-10-18 17:49:00 -0600

    TestTIPDistance.java shouldn't be modified for this pull request of Modify TIPTrip.java....

--- server/src/test/java/com/tripco/t18/TIP/TestTIPDistance.java
+++ server/src/test/java/com/tripco/t18/TIP/TestTIPDistance.java
@@ -21,7 +21,6 @@ public class TestTIPDistance {
 
   private final float radiusMiles = 3958;
   private Map<String, Object> csu;
-  private Map<String, Object> denver;
   private final int version = 1;
 
   @Before
@@ -30,30 +29,15 @@ public class TestTIPDistance {
     csu.put("latitude", "40.576179");
     csu.put("longitude", "-105.080773");
     csu.put("name", "Oval, Colorado State University, Fort Collins, Colorado, USA");
-
-    denver = new HashMap<>();
-    denver.put("latitude", "39.6815");
-    denver.put("longitude", "-104.963");
-    denver.put("name", "CU Denver, Denver, Colorado, USA");
   }
 
   @Test
   public void testOriginDestinationSame() {
     TIPDistance trip = new TIPDistance(version, csu, csu, radiusMiles);
-//    trip.buildResponse();
+    trip.buildResponse();
     int expect = 0;
     int actual = trip.getDistance();
     assertEquals("origin and destination are the same", expect, actual);
-
   }
-
-  @Test
-  public  void testOriginDestinationnotEquals(){
-    TIPDistance trip=new TIPDistance(version, csu, denver, radiusMiles);
-//    trip.buildResponse();
-    int expect=62;
-    int actual=trip.getDistance();
-    assertNotEquals("origin and destination are the same",expect,actual);
   }
-}
 
