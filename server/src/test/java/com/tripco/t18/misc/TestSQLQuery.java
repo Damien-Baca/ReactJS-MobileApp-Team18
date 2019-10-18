0449d928cfdc236d1421bd9eb46d74c85189e9a2
kalek91 <cabelein@rams.colostate.edu>
2019-10-18 14:50:29 -0600

    Fixed Travis problems.

--- server/src/test/java/com/tripco/t18/misc/TestSQLQuery.java
+++ server/src/test/java/com/tripco/t18/misc/TestSQLQuery.java
@@ -13,17 +13,17 @@ public class TestSQLQuery {
     Map<String, String>[] queryResult = testQuery.sendQuery("airport", 10);
     int actualCount = Integer.parseInt(queryResult[0].get("found"));
 
-    assertEquals("Expect 5 results.", expectedCount, actualCount);
+    assertEquals("Expect 5 results from \"airport\".", expectedCount, actualCount);
   }
 
   @Test
   public void testLatitudeQuery() {
     SQLQuery testQuery = new SQLQuery();
-    int expectedCount = (testQuery.localDatabase() ? 4 : 223);
+    int expectedCount = (testQuery.localDatabase() ? 9 : 223);
 
     Map<String, String>[] queryResult = testQuery.sendQuery("40", 10);
     int actualCount = Integer.parseInt(queryResult[0].get("found"));
 
-    assertEquals("Expect 4 results.", expectedCount, actualCount);
+    assertEquals("Expect 9 results from \"40\".", expectedCount, actualCount);
   }
 }
