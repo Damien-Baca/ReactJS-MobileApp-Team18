edb62d2124ce5069e91334c1fa5676fc7522d1e9
jacky <chenlong890729@gmail.com>
2019-11-14 19:27:37 -0700

    Save KML

--- client/src/components/Application/DestinationControls.js
+++ client/src/components/Application/DestinationControls.js
@@ -1,172 +1,179 @@
 import React, {Component} from 'react';
 import {Button, Container, Form, FormGroup, Input, Label, Row} from "reactstrap";
+// import { kmlWrite } from './Kml'
 
 export default class DestinationControls extends Component {
-    constructor(props) {
-      super(props);
+  constructor (props) {
+    super(props);
 
-      this.state = {
-        newDestination: {name: '', latitude: '', longitude: ''},
-        valid: {name: false, latitude: false, longitude: false},
-        invalid: {name: false, latitude: false, longitude: false},
-        fileContents: null
-      };
+    this.state = {
+      newDestination: { name: '', latitude: '', longitude: '' ,altitude:''},
+      valid: { name: false, latitude: false, longitude: false ,altidude:false  },
+      invalid: { name: false, latitude: false, longitude: false ,altidude:false},
+      fileContents: null
+    };
   }
 
-  render() {
+  render () {
     return (
-        <Container>
-          <Row>
-            {this.renderConditionalCumulativeDistance()}
-          </Row>
-          <Row>
-            {this.renderAddDestination()}
-          </Row>
-          <Row style={{display:"flex"}} >
-            {this.renderCalculateDistances()}
-            {this.renderOptimizeDistances()}
-          </Row>
-        </Container>
+      <Container>
+        <Row>
+          {this.renderConditionalCumulativeDistance()}
+        </Row>
+        <Row>
+          {this.renderAddDestination()}
+        </Row>
+        <Row style={{ display: "flex" }}>
+          {this.renderCalculateDistances()}
+          {this.renderOptimizeDistances()}
+          {<Button className='btn-csu h-5 w-10'
+                   size={'sm'}
+                   value='MKL'
+                   active={true}
+                   onClick={() => this.kmlWrite()}
+          >Save Kml</Button>}
+        </Row>
+      </Container>
     );
   }
 
-  renderConditionalCumulativeDistance() {
+  renderConditionalCumulativeDistance () {
     if (this.props.distances !== null) {
       return (
-          <Label>Cumulative Trip
-            Distance: {this.props.sumDistances()}</Label>
+        <Label>Cumulative Trip
+          Distance: {this.props.sumDistances()}</Label>
       );
     }
 
     return (
-        <Label>Trip distance not yet calculated.</Label>
+      <Label>Trip distance not yet calculated.</Label>
     );
   }
 
-  renderAddDestination() {
+  renderAddDestination () {
     return (
-        <Form>
-          <FormGroup>
-            <Label for='add_name'>New Destination</Label>
-            {this.generateCoordinateInput()}
-            {this.renderAddDestinationButton()}
-            {this.renderAddUserDestinationButton()}
-            {this.renderJSONInput()}
-            {this.renderAddJSONButton()}
-            {this.renderExportFileButton()}
-          </FormGroup>
-        </Form>
+      <Form>
+        <FormGroup>
+          <Label for='add_name'>New Destination</Label>
+          {this.generateCoordinateInput()}
+          {this.renderAddDestinationButton()}
+          {this.renderAddUserDestinationButton()}
+          {this.renderJSONInput()}
+          {this.renderAddJSONButton()}
+          {this.renderExportFileButton()}
+        </FormGroup>
+      </Form>
     );
   }
 
-  renderExportFileButton() {
+  renderExportFileButton () {
     return (
-        <Button
-            className='btn-csu w-100 text-left'
-            name='exportFile'
-            key='button_exportFile'
-            active={true}
-            onClick={() => this.props.handleExportFile()}>
-          Export Trip to File
-        </Button>
+      <Button
+        className='btn-csu w-100 text-left'
+        name='exportFile'
+        key='button_exportFile'
+        active={true}
+        onClick={() => this.props.handleExportFile()}>
+        Export Trip to File
+      </Button>
     );
   }
 
-  renderCalculateDistances() {
+  renderCalculateDistances () {
     return (
-        <Button
-            className='btn-csu'
-            name='calculate'
-            style={{marginLeft: 'auto'}}
-            onClick={() => this.props.calculateDistances('none')}
-            disabled={this.props.destinations.length === 0}
-            active={true}
-        >Calculate Trip Distances</Button>
+      <Button
+        className='btn-csu'
+        name='calculate'
+        style={{ marginLeft: 'auto' }}
+        onClick={() => this.props.calculateDistances('none')}
+        disabled={this.props.destinations.length === 0}
+        active={true}
+      >Calculate Trip Distances</Button>
     );
   }
 
-  renderOptimizeDistances() {
-      return (
-          <Button
-              className='btn-csu'
-              name='optimize'
-              style={{marginLeft: 'auto'}}
-              onClick={() => this.props.calculateDistances(
-                  this.props.optimization)}
-              disabled={this.props.destinations.length === 0 ||
-              this.props.optimization === 'none'}
-              active={true}
-          >Optimize Trip Distances</Button>
-      );
-  }
-
-  renderAddDestinationButton() {
+  renderOptimizeDistances () {
     return (
-        <Button
-            className='btn-csu w-100 text-left'
-            name='add_new_destination'
-            key='button_add_destination'
-            active={true}
-            onClick={() => this.handleNewDestination()}
-            disabled={!(this.state.valid.latitude && this.state.valid.longitude)
-            || (this.state.newDestination.name === '')}
-        >Add New Destination</Button>
+      <Button
+        className='btn-csu'
+        name='optimize'
+        style={{ marginLeft: 'auto' }}
+        onClick={() => this.props.calculateDistances(
+          this.props.optimization)}
+        disabled={this.props.destinations.length === 0 ||
+        this.props.optimization === 'none'}
+        active={true}
+      >Optimize Trip Distances</Button>
     );
   }
 
-  renderAddUserDestinationButton() {
+  renderAddDestinationButton () {
     return (
-        <Button
-            className='btn-csu w-100 text-left'
-            name='add_user_destination'
-            key='button_add_user_destination'
-            active={true}
-            onClick={() => this.handleUserDestination()}>
-          Add User Location
-        </Button>
+      <Button
+        className='btn-csu w-100 text-left'
+        name='add_new_destination'
+        key='button_add_destination'
+        active={true}
+        onClick={() => this.handleNewDestination()}
+        disabled={!(this.state.valid.latitude && this.state.valid.longitude&& this.state.valid.altidude)
+        || (this.state.newDestination.name === '')}
+      >Add New Destination</Button>
     );
   }
 
-  renderJSONInput() {
+  renderAddUserDestinationButton () {
     return (
-        <Input type='file'
-               id='fileItem'
-               key='input_json_file'
-               name='json_file'
-               onChange={event => this.onFileChange(event)}/>
+      <Button
+        className='btn-csu w-100 text-left'
+        name='add_user_destination'
+        key='button_add_user_destination'
+        active={true}
+        onClick={() => this.handleUserDestination()}>
+        Add User Location
+      </Button>
     );
   }
 
-  renderAddJSONButton() {
+  renderJSONInput () {
     return (
-        <Button
-            className='btn-csu w-100 text-left'
-            name='loadJSON'
-            key='button_loadJSON'
-            active={true}
-            disabled={this.state.fileContents === null}
-            onClick={() => this.props.handleLoadJSON(this.state.fileContents)}>
-          Import JSON
-        </Button>
+      <Input type='file'
+             id='fileItem'
+             key='input_json_file'
+             name='json_file'
+             onChange={event => this.onFileChange(event)}/>
     );
   }
 
-  generateCoordinateInput() {
+  renderAddJSONButton () {
+    return (
+      <Button
+        className='btn-csu w-100 text-left'
+        name='loadJSON'
+        key='button_loadJSON'
+        active={true}
+        disabled={this.state.fileContents === null}
+        onClick={() => this.props.handleLoadJSON(this.state.fileContents)}>
+        Import JSON
+      </Button>
+    );
+  }
+
+  generateCoordinateInput () {
     return (Object.keys(this.state.newDestination).map((field) => (
-        <Input type='text'
-               key={'input_' + field}
-               name={field}
-               id={`add_${field}`}
-               placeholder={field.charAt(0).toUpperCase() + field.substring(1,
-                   field.length)}
-               value={this.state.newDestination[field]}
-               valid={this.state.valid[field]} //THIS.STATE.VALID[FIELD]
-               invalid={this.state.invalid[field]}
-               onChange={(event) => this.updateNewDestinationOnChange(event)}/>
+      <Input type='text'
+             key={'input_' + field}
+             name={field}
+             id={`add_${field}`}
+             placeholder={field.charAt(0).toUpperCase() + field.substring(1,
+               field.length)}
+             value={this.state.newDestination[field]}
+             valid={this.state.valid[field]} //THIS.STATE.VALID[FIELD]
+             invalid={this.state.invalid[field]}
+             onChange={(event) => this.updateNewDestinationOnChange(event)}/>
     )));
   }
 
-  updateNewDestinationOnChange(event) {
+  updateNewDestinationOnChange (event) {
     if (event.target.value === '' || event.target.name === 'name') { //empty or field is name
       this.setValidState(event.target.name, event.target.value, false, false);
     } else if (this.props.validation(event.target.name, event.target.value)) { //if coord is good
@@ -176,31 +183,31 @@ export default class DestinationControls extends Component {
     }
   }
 
-  handleNewDestination() {
-      if(this.props.validation(this.state.newDestination.latitude, this.state.newDestination.longitude)) {
-        this.props.addDestinations([Object.assign({}, this.state.newDestination)]);
-        let superFalse = {latitude: false, longitude: false};
-        this.setState({
-          newDestination: {name: '', latitude: '', longitude: ''},
-          valid: superFalse,
-          invalid: superFalse
-        });
-        this.props.resetDistances();
-      } else {
-        this.props.setErrorBanner(this.props.createErrorBanner(
-              'Invalid Coordinates',
-              "400",
-              'Invalid Cardinal Coordinates')
-        )
-      }
+  handleNewDestination () {
+    if (this.props.validation(this.state.newDestination.latitude, this.state.newDestination.longitude,this.state.newDestination.altitude)) {
+      this.props.addDestinations([Object.assign({}, this.state.newDestination)]);
+      let superFalse = { latitude: false, longitude: false ,altidude:false};
+      this.setState({
+        newDestination: { name: '', latitude: '', longitude: '' , altitude: ''},
+        valid: superFalse,
+        invalid: superFalse
+      });
+      this.props.resetDistances();
+    } else {
+      this.props.setErrorBanner(this.props.createErrorBanner(
+        'Invalid Coordinates',
+        "400",
+        'Invalid Cardinal Coordinates')
+      )
+    }
   }
 
-  handleUserDestination() {
+  handleUserDestination () {
     this.props.addDestinations([Object.assign({}, this.props.userLocation)]);
     this.props.resetDistances();
   }
 
-  setValidState(name, value, valid, invalid) {
+  setValidState (name, value, valid, invalid) {
     let update = Object.assign({}, this.state.newDestination);
     update[name] = value;
     let cloneValid = Object.assign({}, this.state.valid);
@@ -214,9 +221,9 @@ export default class DestinationControls extends Component {
     });
   }
 
-  onFileChange(event) {
+  onFileChange (event) {
     let callback = (string) => {
-      this.setState({fileContents: string});
+      this.setState({ fileContents: string });
     };
     let fileIn = event.target;
     if (fileIn) {
@@ -230,4 +237,69 @@ export default class DestinationControls extends Component {
       reader.readAsText(file);
     }
   }
+
+
+  kmlWrite () {
+    console.log("00000000000000000",this.props.destinations)
+
+
+    let file = " ";
+    // var location = ["Colorado", "Denver", "Fort Collins", "Loveland", "Boulder", "California", "New York", "China", "Bangladesh"]
+    // var latitude = ["40.5749824", "105.0838675 ", "940.5749824", "1805.0838675 ", "840.5749824", "1805.0838675", "480.574984", "8105.083675", "480.574824"]
+    // var longitude = ["1805.083875", "040.574924", "1905.03865", "460.54984", "8105.08375", "440.574824", "7105.088675 ", "450.5749824", "205.0838675"]
+    // var altitude = ["40.5749824", "105.0838675 ", "940.5749824", "1805.0838675 ", "840.5749824", "1805.0838675", "480.574984", "8105.083675", "480.574824"]
+
+    // var location = this.props.destinations.name()
+    // var latitude = this.props.destinations.latitude()
+    // var longitude = this.props.destinations.longitude()
+    // var altitude = 'none'
+
+    var start = "<?xml version=" + "\"1.0\"" + " encoding=" + "\"UTF-8\"" + "?> \n\
+<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n";
+
+    // const fs = require('fs')
+    //fs.appendFile('trip.kml', start, (err) => {if (err) throw err;})
+
+    // var j;
+    // for (i = 0; i < location.length; i++) {
+
+    var j = "<Document>\n\
+\
+"
+    var list="";
+    for (var i = 0; i < this.props.destinations.length; i++) {
+
+      j = j.concat("\t<Placemark>\n\
+ \t\t<name>" + this.props.destinations[i].name+"</name>\n\
+\t\t<Point>\n\
+\t\t\t<coordinates>"+"\n\t\t\t" +this.props.destinations[i].latitude + "," + this.props.destinations[i].longitude + ","+this.props.destinations[i].altitude + "\n\t\t\t</coordinates>\n\
+\t\t</Point>\n\
+\t</Placemark>\n\
+");
+      list=list.concat( "\n\t\t\t"+this.props.destinations[i].latitude + "," + this.props.destinations[i].longitude + ","+this.props.destinations[i].altitude )
+      console.log(list)
+    }
+
+    var bcd = "\
+\t<Placemark>\n\
+\t\t<name>trip lines</name>>\n\
+\t\t<LineSring>\n\
+\t\t\t<coordinates>" + list+ "\n\t\t\t</coordinates>\n\
+\t\t</LineSring>\n\
+\t</Placemark>\n\
+</Document>\n" + "</kml>";
+
+    file = start + j + bcd;
+    console.log(file);
+
+    var data = new Blob([file], { type: 'text/plain' });
+    var URL = window.URL.createObjectURL(data);
+    var tempLink = document.createElement('a');
+    tempLink.href = URL;
+    tempLink.setAttribute('download', 'Trip.KML');
+    tempLink.click();
+
+  }
 }
+
+
