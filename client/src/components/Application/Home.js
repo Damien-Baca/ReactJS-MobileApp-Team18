6335da314bfee8fe96782cf10a37cfb8012df031
jameslouns <james.lounsbury0804@gmail.com>
2019-12-05 11:29:11 -0700

    added schema validation for loading files in handleLoadJSON also removed check for no file in handleLoadJSON since already handled by not allowing to click button if no file loaded

--- client/src/components/Application/Home.js
+++ client/src/components/Application/Home.js
@@ -271,25 +271,26 @@ export default class Home extends Component {
   }
 
   handleLoadJSON(fileContents) {
-
-    if (fileContents) {
-      try {
-        let newTrip = JSON.parse(fileContents);
+    let AJV = require('ajv');
+    let ajv = new AJV();
+    let schema = require('../../../schemas/TIPTripFileSchema');
+    try {
+      let newTrip = JSON.parse(fileContents);
+      if (ajv.validate(schema, newTrip)) {
         this.addJsonValues(newTrip);
-
-      } catch (e) {
-        this.setErrorBanner( this.props.createErrorBanner(
-              "File Error",
-              0,
-              "File has invalid JSON TIP Trip format."
-          ));
-      }
-    } else {
-      this.setErrorBanner( this.props.createErrorBanner(
+      } else {
+        this.setErrorBanner(this.props.createErrorBanner(
             "File Error",
             0,
-            "No file has been selected."
+            "File has invalid JSON TIP Trip format."
         ));
+      }
+    } catch (e) {
+      this.setErrorBanner(this.props.createErrorBanner(
+          "File Error",
+          0,
+          "File has invalid JSON TIP Trip format."
+      ));
     }
   }
 
